9721c8e398e34008e98c557c5ee1a4a7a4c8e736
Rasul Heidari <rasul.heidari@osapiens.com>
2025-06-02 21:10:34 +0200

    fix route

--- server/vite.ts
+++ server/vite.ts
@@ -70,18 +70,18 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "../dist");
+  const distPublicPath = path.resolve(__dirname, "../dist/public");
 
-  if (!fs.existsSync(distPath)) {
+  if (!fs.existsSync(distPublicPath)) {
     throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
+      `Could not find the build directory: ${distPublicPath}, make sure to build the client first`,
     );
   }
 
-  app.use(express.static(distPath));
+  app.use(express.static(distPublicPath));
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+    res.sendFile(path.resolve(distPublicPath, "index.html"));
   });
 }
